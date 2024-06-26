b6d10aeff89e1e2a5246a2898f1cc141ea162c4f
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-06-26 11:04:55 +0200

    Update install_nextcloud.sh

--- install_nextcloud.sh
+++ install_nextcloud.sh
@@ -4,47 +4,49 @@
 #########################################################
 
 # Maj de Debian
+echo "Maj de votre Debian... please wait ! "
 sleep 3
 apt update -y && apt upgrade -y
+clear
 
 # installation des dependances fonctionnelles 
+echo " Installation des dependances fonctionnelles"
 sleep 3
-echo " installation des dependances fonctionnelles"
 apt-get install apache2 libapache2-mod-php mariadb-server php-xml php-cli php-cgi php-mysql php-mbstring php-gd php-curl php-zip wget unzip -y
 clear
 
 # on relance apache et mariadb
-sleep 3
 echo "on relance apache et mariadb"
+sleep 3
 /etc/init.d/apache2 restart
 /etc/init.d/mysql restart
 
 # creation de la base de données
-sleep 3
 echo "creation de la base de données"
+sleep 3
 mysql -u root -e "create database nextclouddb;"
 mysql -u root -e "grant all privileges on nextclouddb.* to 'usernextcloud'@'localhost' identified by 'operations';"
 mysql -u root -e "flush privileges;"
 
 # téléchargement de NextCloud
-sleep 3
 echo "Téléchargement de NextCloud... please wait !"
+sleep 3
 curl -o nextcloud.zip https://download.nextcloud.com/server/releases/latest.zip
 
 # Dezippe de l'archive
-sleep 3
 echo "On dezippe l'archive..."
+sleep 3
 unzip nextcloud.zip
 clear
 
 # on déplace Nextcloud dans /var/www/html
-sleep 3
 echo "on déplace Nextcloud dans /var/www/html"
+sleep 3
 mv nextcloud /var/www/html
 clear
 
 # On met les droits sur le dossier nextcloud
-sleep 3
 echo "On met les droits sur le dossier nextcloud"
+sleep 3
 chown -R www-data:www-data /var/www/html/nextcloud
 echo " Installation de Nextcloud terminé !"
